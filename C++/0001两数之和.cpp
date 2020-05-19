b97ebfcc4404b2f6ad76ba6ce07eae8d47e098f4
KingR-GH <wzx979597842@qq.com>
2020-05-19 20:45:08 +0800

    Create 0001两数之和.cpp

---
+++ C++/0001两数之和.cpp
@@ -0,0 +1,22 @@
+﻿//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+//你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map <int, int > map;
+        vector<int> res;
+        for (int i = 0; i < nums.size(); i++) {
+            int n = target - nums[i];
+            if (map.find(n) != map.end()) {
+                res.push_back(map[n]);
+                res.push_back(i);
+                break;
+            }
+            else {
+                map[nums[i]] = i;
+            }
+        }
+        return res;
+    }
+};
