247584e02ae9c72aae525e14a12f344bb0a13518
KingR <wzx979597842@qq.com>
2020-07-20 07:55:56 +0800

    7/20/2020

---
+++ C#/0167两数之和II-输入有序数组.cs
@@ -0,0 +1,39 @@
+﻿/*给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+说明:
+
+返回的下标值（index1 和 index2）不是从零开始的。
+你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/*
+可以用双指针法,
+分别指向头尾元素,如果两元素的和大于目标,尾指针前移,如果小于目标,头指针后移,等于目标即可得答案
+*/
+
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        if(numbers.Length == 0){return null;}
+        int[] ret = new int[2];
+        int p1 = 0,p2 = numbers.Length - 1;
+        while(p1 < p2){
+            int temp = numbers[p1] + numbers[p2];
+            if(temp == target){
+                ret = new int[2]{p1+1,p2+1};
+                break;
+            }
+            else if(temp < target){
+                p1++;
+            }
+            else if(temp > target){
+                p2--;
+            }
+        }
+        return ret;
+    }
+}
+
